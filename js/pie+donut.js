f5cc4b97660405d1d61d28f380f421acb866a6a5
Tian Jiang <riersy2324@gmail.com>
2019-06-24 12:56:33 +0800

    Update pie+donut.js

--- js/pie+donut.js
+++ js/pie+donut.js
@@ -1,45 +1,102 @@
-var width = 400, height = 400, margin = 30;
+var width = 400,
+  height = 400,
+  margin = 30;
 
 var radius = Math.min(width, height) / 2 - margin;
 
-var svg = d3.select("#my_datavis")
+var svg = d3
+  .select("#my_datavis")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
   .append("g")
   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var data = {a: 9, b: 20, c:30, d:8, e:12}
+var data = { a: 1, b: 3, c: 5, d: 8, e: 12 };
 
-var color = ["#D8E2DC", "#FFE5D9", "#FFCAD4", "#F4ACB7", "#9D8189"]
-var color2 = ["#1A535C", "#4ECDC4", "#B8B8D1", "#FF6B6B", "#FFE66D"]
+var color = ["#D8E2DC", "#FFE5D9", "#FFCAD4", "#F4ACB7", "#9D8189"];
+var color2 = ["#1A535C", "#4ECDC4", "#B8B8D1", "#FF6B6B", "#FFE66D"];
 
+var pie = d3.pie().value(function(d) {
+  return d.value;
+});
+var data_ready = pie(d3.entries(data));
 
-var pie = d3.pie()
-  .value(function(d) {return d.value; })
-var data_ready = pie(d3.entries(data))
-
-var arcGenerator = d3.arc()
+var arcGenerator = d3
+  .arc()
   .innerRadius(0)
-  .outerRadius(radius-80)
-var arcGenerator2 = d3.arc()
+  .outerRadius(radius - 80);
+var arcGenerator2 = d3
+  .arc()
   .innerRadius(100)
-  .outerRadius(radius)
+  .outerRadius(radius);
 
-svg.selectAll('mySlices')
+svg
+  .selectAll("mySlices")
   .data(data_ready)
   .enter()
-  .append('path')
-    .attr('d', arcGenerator)
-    .attr("stroke", "none")
-    .attr("fill", function(d,i) { return color[i]})
-    .style("stroke-width", "2px")
+  .append("path")
+  .attr("d", arcGenerator)
+  .attr("stroke", "none")
+  .attr("fill", function(d, i) {
+    return color[i];
+  })
+  .style("stroke-width", "2px")
+	.on("mouseover", function(d){return tooltip.style("visibility", "visible");})
+	.on("mousemove", function(d){return tooltip.html(d.data.key + ": " + d.data.value).style("top", (event.pageY + 20)+"px").style("left",(event.pageX + 20)+"px");})
+	.on("mouseout", function(d){return tooltip.style("visibility", "hidden");});
 
-svg.selectAll('mySlices')
+svg
+  .selectAll("mySlices")
   .data(data_ready)
   .enter()
-  .append('path')
-    .attr('d', arcGenerator2)
-    .attr("stroke", "none")
-    .attr("fill", function(d,i) { return color2[i]})
-    .style("stroke-width", "2px")
+  .append("text")
+  .text(function(d) {
+    return d.data.key;
+  })
+  .attr("transform", function(d) {
+    return "translate(" + arcGenerator.centroid(d) + ")";
+  })
+  .style("text-anchor", "middle")
+  .style("font-size", 17);
+
+svg
+  .selectAll("mySlices")
+  .data(data_ready)
+  .enter()
+  .append("path")
+  .attr("d", arcGenerator2)
+  .attr("stroke", "none")
+  .attr("fill", function(d, i) {
+    return color2[i];
+  })
+  .style("stroke-width", "2px")
+	.on("mouseover", function(d){return tooltip.style("visibility", "visible");})
+	.on("mousemove", function(d){return tooltip.html(d.data.key + ": " + d.data.value).style("top", (event.pageY + 20)+"px").style("left",(event.pageX + 20)+"px");})
+	.on("mouseout", function(d){return tooltip.style("visibility", "hidden");});
+
+svg
+  .selectAll("mySlices")
+  .data(data_ready)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d.data.key;
+  })
+  .attr("transform", function(d) {
+    return "translate(" + arcGenerator2.centroid(d) + ")";
+  })
+  .style("text-anchor", "middle")
+  .style("font-size", 17);
+
+var tooltip = d3.select("body")
+	.append("div")
+	.style("position", "absolute")
+	.style("z-index", "10")
+	.style("visibility", "hidden")
+  .style("background-color", "#E8EEED")
+  .style("padding", "5px")
+  .style("border", "solid")
+  .style("border-color", "#D2DEDB")
+  .style("border-width", "0.5px")
+  .style("border-radius", "5px");
